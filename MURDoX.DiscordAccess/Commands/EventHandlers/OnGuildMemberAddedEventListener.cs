0973a3fc0797e01db4db808e958f5a1073c0c504
g-larose <larose.g@fromtier.com>
2023-02-12 12:32:03 -0500

    added EmbedBuilderHelper class and changed OnGuildMemberAdded to async

--- MURDoX.DiscordAccess/Commands/EventHandlers/OnGuildMemberAddedEventListener.cs
+++ MURDoX.DiscordAccess/Commands/EventHandlers/OnGuildMemberAddedEventListener.cs
@@ -1,5 +1,7 @@
 ﻿using DSharpPlus;
 using DSharpPlus.EventArgs;
+using MURDoX.Core.Helpers;
+using MURDoX.Core.Models;
 using MURDoX.Core.Models.Utility.WelcomeService;
 using MURDoX.Core.Services;
 
@@ -7,19 +9,23 @@ namespace MURDoX.DiscordAccess.Commands.EventHandlers
 {
     public class OnGuildMemberAddedEventListener
     {
-        internal Task OnGuildMemberAdded(DiscordClient sender, GuildMemberAddEventArgs e)
+        internal async Task OnGuildMemberAdded(DiscordClient sender, GuildMemberAddEventArgs e)
         {
+            EmbedBuilderHelper embedBuilder = new();
             WelcomeService welcomeService = new();
             WelcomeServiceInput welcomeServiceInput = new()
             {
                 Username = e.Member.Username
             };
             WelcomeServiceResponse response = welcomeService.GetWelcomeMessage(welcomeServiceInput).Result;
-            // TODO: Send the message to the user or to the welcome channel of the server
-
-            e.Guild.GetDefaultChannel().SendMessageAsync(response.Message);
-
-            return Task.CompletedTask;
+            Embed embed = new Embed()
+            {
+                Title = $"{e.Member.Username} welcome to {e.Guild.Name}",
+                Color = await ShuffleHelper.GetRandomEmbedColorAsync(),
+                Desc = $"{response.Message}"
+            };
+            await e.Guild.GetDefaultChannel().SendMessageAsync(response.Message);
+            await e.Member.SendMessageAsync(embed: embedBuilder.Build(embed));
         }
     }
 }
